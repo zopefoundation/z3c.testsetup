51eb63fda4255436b4f4e530b205baa7df58fa01
Uli Fouquet <uli@gnufix.de>
2010-09-14 23:31:34 +0000

    Set up layered setup tests with ZCML layers allowed to be torn down. This way we (hopefully) make the Windows tests pass again. While the tests defined in the layered testsetup do not need to be run in separated subprocesses, this might be an issue in real-world applications. But it seems not to be something we can handle inside z3c.testsetup.
    git-svn-id: file:///home/mg/src/zope-git-migration/zope-mirror/z3c.testsetup/trunk@116421 62d5b8a3-27da-0310-9561-8e5933582275

--- src/z3c/testsetup/tests/layered_cave/layeredsetup01.py
+++ src/z3c/testsetup/tests/layered_cave/layeredsetup01.py
@@ -1,2 +1,3 @@
 from z3c.testsetup import register_all_tests
-test_suite = register_all_tests('z3c.testsetup.tests.layered_cave')
+test_suite = register_all_tests('z3c.testsetup.tests.layered_cave',
+                                allow_teardown=True)
